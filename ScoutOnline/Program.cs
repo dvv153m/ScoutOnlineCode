f948e056218769f3c13aed7c5e68358448c1ffa7
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-11-03 19:01:25 +0300

    refactoring maps

--- ScoutOnline/Program.cs
+++ ScoutOnline/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ScoutOnline.Core.Map;
 
 namespace ScoutOnline
 {
@@ -15,9 +16,10 @@ namespace ScoutOnline
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddSingleton<MapsService>();
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });            
 
             await builder.Build().RunAsync();
         }
