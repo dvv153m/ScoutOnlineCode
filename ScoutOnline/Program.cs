285a894f5b84e0fed060ab562d7baa9656ba30c9
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-11-18 23:44:36 +0300

    pwa + delete auth

--- ScoutOnline/Program.cs
+++ ScoutOnline/Program.cs
@@ -32,10 +32,38 @@ namespace ScoutOnline
             
                 new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
                 
-            );
+            );            
 
             //string serverlessBaseURI = builder.Configuration["BaseURI"];
             await builder.Build().RunAsync();
         }
     }
 }
+
+//todo
+//pwa каждый раз заново загружает приложение из кеша не загружается
+//добавить combobox telerik на выбор карт
+//подрефачить аутентификацию index.razor удалить model
+//попробовать при создании проекта поставить галочку у авторизации
+//онлайн данные чоб загружались один раз
+//вывод телериковской таблички
+//при обновлении страницы редиректит на форму логин
+//вывод отчета на js код взять из stimulsoft
+//вывод на карте объектов и геозон
+//signalr + blazor
+
+//добавление telerik
+//1) через нугет скачиваем Telerik.UI.for.Blazor.Trial
+//2) в index.html добавляем 
+//<link rel="stylesheet" href="_content/Telerik.UI.for.Blazor.Trial/css/kendo-theme-default/all.css" />
+//< script src = "_content/Telerik.UI.for.Blazor.Trial/js/telerik-blazor.js" defer ></ script >
+//3) в Imports.razor добавить 
+//@using Telerik.Blazor
+//@using Telerik.Blazor.Components
+//5) добавить в MainLayout.razor
+//@inherits LayoutComponentBase
+//<TelerikRootComponent>
+//.....
+//</TelerikRootComponent >
+//6) Program.cs
+//builder.Services.AddTelerikBlazor();
