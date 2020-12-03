921b9af3e114a46260fe82c9f7dfe1025a72778c
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-12-03 19:14:15 +0300

    add charts final

--- BlazorChart/ChartInterop.cs
+++ BlazorChart/ChartInterop.cs
@@ -1,13 +1,15 @@
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
+using BlazorChart.Models;
 
 namespace BlazorChart
 {
     public class ChartInterop
     {        
-        public static ValueTask Init(IJSRuntime jsRuntime, string id)
-        {
-            return jsRuntime.InvokeVoidAsync("chartBlazor.init", id);
+        public static ValueTask Init(IJSRuntime jsRuntime, ChartModel[] chartModels)
+        {            
+            var chartModelsJson = System.Text.Json.JsonSerializer.Serialize(chartModels);
+            return jsRuntime.InvokeVoidAsync("chartBlazor.init", chartModelsJson);
         }
     }
 }
--- BlazorChart/wwwroot/chartInterop.js
+++ BlazorChart/wwwroot/chartInterop.js
@@ -1,9 +1,11 @@
 // This file is to show how a library package may provide JavaScript interop features
 // wrapped in a .NET API
 
+charts = [];
+
 window.chartBlazor = {
     
-    init: function (idChart) {
+    init: function (chartModelsJson) {
         
         am4core.ready(function () {
 
@@ -11,39 +13,67 @@ window.chartBlazor = {
             am4core.useTheme(am4themes_material);
             am4core.useTheme(am4themes_animated);
             // Themes end
+            
+            var chartModels = JSON.parse(chartModelsJson);
 
-            // Create chart instance
-            var chart = am4core.create(idChart, am4charts.XYChart);            
+            chartModels.forEach((element) => {
+                
+                charts.push(createChart(element.Id));                  
+            })
 
-            // Add data
-            chart.data = generateChartData();
+            function createChart(idChart) {
 
-            // Create axes
-            var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-            dateAxis.renderer.minGridDistance = 50;
+                // Create chart instance
+                var chart = am4core.create(idChart, am4charts.XYChart);
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                // Add data
+                chart.data = generateChartData();
 
-            // Create series
-            var series = chart.series.push(new am4charts.LineSeries());
-            series.dataFields.valueY = "visits";
-            series.dataFields.dateX = "date";
-            series.strokeWidth = 2;
-            series.minBulletDistance = 10;
-            series.tooltipText = "{valueY}";
-            series.tooltip.pointerOrientation = "vertical";
-            series.tooltip.background.cornerRadius = 20;
-            series.tooltip.background.fillOpacity = 0.5;
-            series.tooltip.label.padding(12, 12, 12, 12)
+                // Create axes
+                var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+                dateAxis.renderer.minGridDistance = 50;
+                //подписка на изменение масштаба и скрол графика
+                dateAxis.events.on("startendchanged", dateAxisChanged);
+                function dateAxisChanged(ev) {
+                    
+                    charts.forEach((element) => {
+                        
+                        element.cursor.xAxis.start = ev.target.start;
+                        element.cursor.xAxis.end = ev.target.end;
+                    })
+                }
 
-            // Add scrollbar
-            chart.scrollbarX = new am4charts.XYChartScrollbar();
-            chart.scrollbarX.series.push(series);
+                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-            // Add cursor
-            chart.cursor = new am4charts.XYCursor();
-            chart.cursor.xAxis = dateAxis;
-            chart.cursor.snapToSeries = series;
+                // Create series
+                var series = chart.series.push(new am4charts.LineSeries());
+                series.dataFields.valueY = "visits";
+                series.dataFields.dateX = "date";
+                series.strokeWidth = 2;
+                series.minBulletDistance = 10;
+                series.tooltipText = "{valueY}";
+                series.tooltip.pointerOrientation = "vertical";
+                series.tooltip.background.cornerRadius = 20;
+                series.tooltip.background.fillOpacity = 0.5;
+                series.tooltip.label.padding(12, 12, 12, 12);
+                series.minBulletDistance = 5;
+
+                //рисование точек на графике
+                var bullet = series.bullets.push(new am4charts.CircleBullet());
+                bullet.circle.stroke = am4core.color("#fff");
+                bullet.circle.strokeWidth = 1;
+
+                // Add scrollbar
+                chart.scrollbarX = new am4charts.XYChartScrollbar();
+                chart.scrollbarX.series.push(series);
+
+                // Add cursor
+                chart.cursor = new am4charts.XYCursor();
+                chart.cursor.xAxis = dateAxis;
+                chart.cursor.snapToSeries = series;
+
+                return chart;
+            }
         });
     }
 };
